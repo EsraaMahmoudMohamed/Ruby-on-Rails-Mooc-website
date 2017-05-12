e6b3ae82a335d4429b7d57ab2a0732d09d76094a
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-05-12 23:29:59 +0200

    add model user &course to dashboard

--- project1/app/admin/admin_user.rb
+++ project1/app/admin/admin_user.rb
@@ -1,9 +1,10 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :name
 
   index do
     selectable_column
-    id_column
+    id_column  
+    column :name
     column :email
     column :current_sign_in_at
     column :sign_in_count
@@ -21,6 +22,7 @@ ActiveAdmin.register AdminUser do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :name
     end
     f.actions
   end
---
+++ project1/db/migrate/20170512204302_add_column_name_to_admin_users.rb
@@ -0,0 +1,5 @@
+class AddColumnNameToAdminUsers < ActiveRecord::Migration
+  def change
+     add_column :admin_users, :name, :string
+  end
+end
