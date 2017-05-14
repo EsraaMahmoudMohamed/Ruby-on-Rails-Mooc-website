4c390a4d7caa993db34a1b56cf18dec2b61a166f
EsraaMahmoudMohamed <esraamahmoud378@gmail.com>
2017-05-14 23:52:37 +0200

    add new user from admin

--- project1/app/admin/user.rb
+++ project1/app/admin/user.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register User do
   permit_params :name, :dob, :picture, :gender, :password, :email
+
+  form do |f|
+    f.inputs "New User" do
+      f.input :name
+      f.input :gender
+      f.input :dob
+      f.input :picture
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+
+    end
+    f.actions
+  end
 end
